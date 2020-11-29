0bbd661108b83e445d23e36b216d64ee73d6bc7d
Aryan <aryanece14@gmail.com>
2020-11-29 16:09:00 +0530

    Link List

---
+++ llst.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+
+     public:
+     int data;
+     Node *nextptr;
+
+
+}; 
+
+int main()
+{
+
+  cout<<"Hello Link list"<<endl;
+
+}
+
+
+
