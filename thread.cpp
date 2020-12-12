afb223154ab0fd98681f5282648901102ac5f55b
Aryan <aryanece14@gmail.com>
2020-12-12 21:14:39 +0530

    multithread with mutex in c++

--- thread.cpp
+++ thread.cpp
@@ -30,18 +30,18 @@ int main()
      
 
      std::thread th1(func,1);
-     //std::thread th2(func,2);
-     //std::thread th3(func,3);
-     //std::thread th4(func,4);
-     //std::thread th5(func,5);
-     //std::thread th6(func,6);
-      
-     th1.join();
-     //th2.join();
-     //th3.join();      
-     //th4.join();
-     //th5.join();
-     //th6.join();
+     std::thread th2(func,2);
+     std::thread th3(func,3);
+     std::thread th4(func,4);
+     std::thread th5(func,5);
+     std::thread th6(func,6);
+     //th1.detach();
+     th1.join(); 
+     th2.join();
+     th3.join();      
+     th4.join();
+     th5.join();
+     th6.join();
   return 0;
 }
 
