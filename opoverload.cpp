ad23e2e149743cf9902ccbfb1e386d066f0b6982
Aryan <aryanece14@gmail.com>
2021-01-16 21:26:54 +0530

    Add operator overload file

---
+++ opoverload.cpp
@@ -0,0 +1,9 @@
+// It gives new definition to operator, use with user defined data type.
+// Ex obj1+opbj2 opeartor +(obj1,obj2), obj is object,struct,enum
+// Operator function two type 1- member operator function, 2- Non member function operator.
+// Non member operator function is also two type 1- Global function operator , 2- Friend function operator
+// While overloading = opeartor be precise as deep and shallow copy will come in to picture
+// overloading of IO stream is not possible with global and member opoerator function, so use friend operator function.  
+
+
+
